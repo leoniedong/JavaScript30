b1c13929496d4e16dbd1b6de7c58086ca5be73e3
Leonie Dong <leoniedong@hotmail.com>
2019-08-16 16:00:09 +0800

    finished challenge

--- 03 - CSS Variables/index.js
+++ 03 - CSS Variables/index.js
@@ -4,8 +4,12 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputs = document.querySelectorAll('.controls input')
 
     function handleUpdate(){
-        console.log(this.value) 
+        const suffix = this.dataset.sizing || ''; //if doesn't have data-sizing, have nothing
+        const name = this.name //'spacing'/'blur'/'base'
+        const value = this.value
+        document.documentElement.style.setProperty(`--${name}`, value + suffix)
     }
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate))
+    inputs.forEach(input => input.addEventListener('mouseover', handleUpdate))
 })
